8766af89bf397e6640948b5c3d1ea92cffb1ba9b
Jon Tapanes <jon@Jons-MacBook-Air.local>
2018-10-15 18:45:02 -0400

    added Material-UI & Login button

--- src/App.js
+++ src/App.js
@@ -9,11 +9,27 @@ import Wrapper from './components/Wrapper'
 // import Button from './components/Button'
 import './App.css'
 import API from './utilities/API'
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+//MAterial UI
+
+// function App() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.querySelector('#app'));
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Navbar />
+      <Button variant="contained" color="primary">
+      Login
+    </Button>
       <Carousel />
       <Wrapper>
         <Route exact path='/' component={Splash} />
